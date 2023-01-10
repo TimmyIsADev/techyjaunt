4485c0944cb05d170f11be33d1a4e7752b416b7a
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2023-01-10 09:04:12 +0100

    set up API to handle mailchimp integration

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const app = express();
 const client = require("@mailchimp/mailchimp_marketing");
 const port = process.env.PORT || 3001;
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
 
-// parse application/json
-app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/", express.static("public"));
 
@@ -17,47 +14,54 @@ app.get("/test", async (req, res) => {
   });
 });
 
-app.post("/sub", async (req, res) => {
+app.post("/sub", (req, res) => {
   console.log(req.body);
   const { emailAddress, firstName } = req.body;
   console.log(emailAddress, firstName);
 
-  //   console.log("Attempting Subscription 🧑...");
-  //   if (emailAddress === "" || firstName === "") {
-  //     return res.status(409).json({
-  //       status: "failed",
-  //       message: "Bad Request",
-  //     });
-  //   }
+  console.log("Attempting Subscription 🧑...");
+  if (emailAddress === "" || firstName === "") {
+    return res.status(409).json({
+      status: "failed",
+      message: "Bad Request",
+    });
+  }
 
-  //   client.setConfig({
-  //     apiKey: "cc76d701880736a55899c33b272f08a4-us21",
-  //     server: "us21",
-  //   });
+  client.setConfig({
+    apiKey: "cc76d701880736a55899c33b272f08a4-us21",
+    server: "us21",
+  });
 
-  //   const run = async () => {
-  //     const response = await client.lists.addListMember("732326f5a9", {
-  //       email_address: emailAddress,
-  //       status: "subscribed",
-  //       merge_fields: {
-  //         FNAME: firstName,
-  //       },
-  //     });
+  const run = async () => {
+    const response = await client.lists
+      .addListMember("732326f5a9", {
+        email_address: emailAddress,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+        },
+      })
+      .then((response) => {
+        if (response) {
+          console.log("Subscription Successful😊");
+          return res.status(200).json({
+            status: "ok",
+            message: "Subscription Successful",
+          });
+        }
+      })
+      .catch((error) => {
+        if (error.status) {
+          console.log("Subscription Failed");
+          return res.status(400).json({
+            status: "failed",
+            message: "Subscription Failed",
+          });
+        }
+      });
+  };
 
-  //     if (response.status === 200) {
-  //       return res.status(200).json({
-  //         status: "ok",
-  //         message: "Subscription Successful",
-  //       });
-  //     } else {
-  //       return res.status(409).json({
-  //         status: "failed",
-  //         message: "Subscription Failed",
-  //       });
-  //     }
-  //   };
-
-  //   await run();
+  run();
 });
 
 app.listen(port, () => {
